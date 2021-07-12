84f0a701444ad90491dafb2bed0dd530d2a1eed1
igordbauer <igordbauer@gmail.com>
2021-07-12 13:16:19 -0300

    Little improvement on board printing

--- Chess-system/src/chess/ChessPosition.java
+++ Chess-system/src/chess/ChessPosition.java
@@ -7,7 +7,7 @@ public class ChessPosition {
 	private char column;
 	private int row;
 
-	public ChessPosition(char column, int row) {
+	public ChessPosition(char column, int row) { //
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error Instantiating ChessPosition. Valid values are from a1 to h8 ");
 		}
@@ -24,17 +24,20 @@ public class ChessPosition {
 		return row;
 	}
 
-	protected Position toPosition() {
+	protected Position toPosition() { // método que transforma a coordenada padrão de matriz do java para coordenada
+										// fornecida pelo usuário durante a partida
+
 		return new Position(8 - row, column - 'a');
 	}
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()),8 - position.getRow());
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
+
 	@Override
 	public String toString() {
-		
+
 		return "" + column + row;
-		
-		
+
 	}
 }
